c4181b77f54a59ccc9b39b4079a62225bba06f6f
Spencer M <spencer.g.moffat@gmail.com>
2024-03-05 23:05:18 -0700

    Initialize tutorial Google cloud functions (wrong app)

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJdBSp9giT5OldEJSmJI6BtJwyYB2oJl0",
+  authDomain: "personal-aphasia-testing.firebaseapp.com",
+  databaseURL: "https://personal-aphasia-testing-default-rtdb.firebaseio.com",
+  projectId: "personal-aphasia-testing",
+  storageBucket: "personal-aphasia-testing.appspot.com",
+  messagingSenderId: "191433257647",
+  appId: "1:191433257647:web:1f81c05d7baf8b647f5a5a",
+};
+
+const app = initializeApp(firebaseConfig);
